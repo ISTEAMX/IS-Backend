d99216888575964a55d9265dcd8064dc61fd7a6f
Lukas <lukas.patrikc@yahoo.com>
2026-03-20 16:56:30 +0200

    IST-22:User entity created

---
+++ src/main/java/com/isteamx/university/entity/User.java
@@ -0,0 +1,33 @@
+package com.isteamx.university.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String firstName;
+
+    @Column(nullable = false)
+    String lastName;
+
+    @Column(unique = true ,nullable = false)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false)
+    String role;
+}
