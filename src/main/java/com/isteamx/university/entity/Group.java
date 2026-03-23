9c4373c833fc131a54a0f537116dbf301743d6e0
LukasL <75202961+LukasL2004@users.noreply.github.com>
2026-03-23 18:42:16 +0200

    IST-35: Schedule entity done & linked with all the tables (#14)

--- src/main/java/com/isteamx/university/entity/Group.java
+++ src/main/java/com/isteamx/university/entity/Group.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
-@Table(name = "group")
+@Table(name = "student_groups")
 public class Group {
 
     @Id
@@ -16,4 +18,7 @@ public class Group {
 
     @Column(unique = true,nullable = false)
     private String identifier;
+
+    @OneToMany(mappedBy = "group",cascade = CascadeType.ALL)
+    private List<Schedule> schedule;
 }
---
+++ src/main/java/com/isteamx/university/entity/Schedule.java
@@ -0,0 +1,52 @@
+package com.isteamx.university.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "schedules")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String day;
+
+    @Column(nullable = false)
+    private String startingHour;
+
+    @Column(nullable = false)
+    private String endingHour;
+
+    @JoinColumn(name = "professor_id",referencedColumnName = "id", nullable = false)
+    @ManyToOne
+    @JsonIgnore
+    private Professor professor;
+
+    @JoinColumn(name = "room_id",referencedColumnName = "id", nullable = false)
+    @ManyToOne
+    @JsonIgnore
+    private Room room;
+
+    @JoinColumn(name = "group_id",referencedColumnName = "id", nullable = false)
+    @ManyToOne
+    @JsonIgnore
+    private Group group;
+
+    @JoinColumn(name = "subject_id",referencedColumnName = "id", nullable = false)
+    @ManyToOne
+    @JsonIgnore
+    private Subject subject;
+
+
+}
