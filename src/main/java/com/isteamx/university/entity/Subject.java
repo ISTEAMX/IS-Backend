46e99022cc1fca18a9705221aca083fafcd66f0e
Lukas <lukas.patrikc@yahoo.com>
2026-03-23 18:16:34 +0200

    IST-33: Subject entity done

---
+++ src/main/java/com/isteamx/university/entity/Subject.java
@@ -0,0 +1,26 @@
+package com.isteamx.university.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String activity_type;
+
+}
