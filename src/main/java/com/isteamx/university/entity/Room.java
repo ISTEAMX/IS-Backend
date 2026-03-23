ae709411f4791167b05106dc897bc54c0f2fdc4a
Lukas <lukas.patrikc@yahoo.com>
2026-03-23 17:34:42 +0200

    IST-27: Room entity done

---
+++ src/main/java/com/isteamx/university/entity/Room.java
@@ -0,0 +1,37 @@
+package com.isteamx.university.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "room")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int capacity;
+
+    @Column(nullable = false)
+    private String type;
+
+    @ElementCollection
+    @CollectionTable(name = "room_equipment",joinColumns = @JoinColumn(name = "room_id"))
+    @Column(nullable = false)
+    private List<String> equipment;
+}
