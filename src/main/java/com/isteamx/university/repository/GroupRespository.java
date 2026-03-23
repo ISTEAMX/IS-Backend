bae3139d4e5890a171f04fed7e77bf6628f8c73c
Lukas <lukas.patrikc@yahoo.com>
2026-03-23 20:18:13 +0200

    IST-96: Created DB repositories

---
+++ src/main/java/com/isteamx/university/repository/GroupRespository.java
@@ -0,0 +1,7 @@
+package com.isteamx.university.repository;
+
+import com.isteamx.university.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRespository extends JpaRepository<Group, Long> {
+}
---
+++ src/main/java/com/isteamx/university/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.isteamx.university.repository;
+
+import com.isteamx.university.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
